3dfc713b4dc02f7a7ef1836a6d928c9e943462ef
Francesca-palmeri <francesca.palmeri.c@gmail.com>
2025-03-19 17:00:16 +0100

    fatta configurazione

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using AjaxMvc.Models;
+using AjaxMvc.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        
+        public AccountController(
+            UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, 
+            RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+           
+                var user = new ApplicationUser
+                {
+                    FirstName = registerViewModel.FirstName,
+                    LastName = registerViewModel.LastName,
+                    BirthDate = registerViewModel.BirthDate,
+                    UserName = registerViewModel.EmailAdress,
+                    Email = registerViewModel.EmailAdress
+                };
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+            
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AjaxMvc.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public required string FirstName { get; set; }
+        [Required]
+        public required string LastName { get; set; }
+        [Required]
+        public required DateOnly BirthDate { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAdress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Compare ("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
